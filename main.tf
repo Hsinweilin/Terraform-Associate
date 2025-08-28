b404fd7230afea20f980e50480a5bf5e45013220
Hsinwei Lin <hsinweilin@arizona.edu>
2025-08-28 15:51:55 -0700

    feat: configure Terraform cloud settings for organization and workspace

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ terraform {
       version = "6.10.0"
     }
   }
+  cloud { 
+    organization = "Hsinwei_Terraform" 
+    workspaces { 
+      name = "Terraform_Associate" 
+    } 
+  } 
 }
-
-provider "aws" {
-  region  = "us-east-1"
-  profile = "Hsinwei_Terraform"
-}
-
 
 resource "aws_instance" "my_server" {
   ami           = "ami-00ca32bbc84273381"
